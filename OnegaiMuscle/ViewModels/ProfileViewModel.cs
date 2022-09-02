e12ead209a2b3e66c9b32709da4c21b6b55ab9f1
沙耶 <yhsia@codersai.com>
2022-09-02 12:30:45 +0800

    profile page wip

--- OnegaiMuscle/ViewModels/ProfileViewModel.cs
+++ OnegaiMuscle/ViewModels/ProfileViewModel.cs
@@ -13,7 +13,9 @@ namespace OnegaiMuscle.ViewModels
     public partial class ProfileViewModel : BaseViewModel
     {
         public ObservableCollection<UserProfile> UserProfiles { get; set; } = new();
-        public ICommand DeleteProfileCommand=>new Command<UserProfile>(async (record)=>
+
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        async Task DeleteProfile(UserProfile record)
         {
             var delete = await Shell.Current.DisplayAlert("Confirmation",
                 "Are you sure you want to delete this profile?", "OK", "Cancel");
@@ -21,33 +23,48 @@ namespace OnegaiMuscle.ViewModels
             {
                 await App.Database.DeleteProfileAsync(record);
                 UserProfiles.Remove(record);
+                if (Preferences.Get("LastSelectedUserId", 0) == record.Id)
+                {
+                    var profiles = await App.Database.GetProfilesAsync();
+                    var nextProfileId = (profiles.FirstOrDefault()?.Id)?? 0;
+                    Preferences.Set("LastSelectedUserId", nextProfileId);
+                    MessagingCenter.Send(this, "ProfileSelected", nextProfileId);
+                }
             }
-        });
+        }
 
-        public ICommand GoToUpdateProfilePageCommand => new Command<UserProfile>(async (record) =>
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        async Task UpdateProfile(UserProfile record)
         {
             await Shell.Current.Navigation.PushAsync(new Page()
             {
                 BindingContext = this
             });
-        });
+        }
 
-        public ICommand SelectProfileCommand => new Command<UserProfile>(async (record) =>
-        {
-            MessagingCenter.Send(this,"ProfileSelected",record.Id);
-            await Shell.Current.Navigation.PopAsync();
-        });
-
-        [RelayCommand]
-        async Task CreateProfile(string s)
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        async Task CreateProfile()
         {
             await Shell.Current.GoToAsync(nameof(CreateProfilePage));
+        }
+
+        [RelayCommand(AllowConcurrentExecutions = false)]
+        async Task SelectProfile(UserProfile record)
+        {
+            MessagingCenter.Send(this, "ProfileSelected", record.Id);
+            await Shell.Current.Navigation.PopAsync();
         }
 
         [RelayCommand]
         async Task Appearing()
         {
             var profiles = await App.Database.GetProfilesAsync();
+            if (Preferences.Get("LastSelectedUserId", 0) == 0 && profiles.Any())
+            {
+                var nextProfileId = (profiles.FirstOrDefault()?.Id) ?? 0;
+                Preferences.Set("LastSelectedUserId", nextProfileId);
+                MessagingCenter.Send(this, "ProfileSelected", nextProfileId);
+            }
             UserProfiles.Clear();
             foreach (var userProfile in profiles)
             {
