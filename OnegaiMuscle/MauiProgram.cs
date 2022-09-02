db2574dc2cc5022b3282046beeb5ec619eba594e
沙耶 <yhsia@codersai.com>
2022-09-02 10:49:46 +0800

    fix windows crash

--- OnegaiMuscle/MauiProgram.cs
+++ OnegaiMuscle/MauiProgram.cs
@@ -8,35 +8,43 @@ namespace OnegaiMuscle;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseSkiaSharp()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			})
-			.UseMauiCompatibility()
-            .ConfigureMauiHandlers((handlers) => {
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseSkiaSharp()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            })
+#warning UseMauiCompatibility will cause exception occurs in Windows platform
 #if ANDROID
+            .UseMauiCompatibility()
+            .ConfigureMauiHandlers((handlers) =>
+            {
                 handlers.AddCompatibilityRenderer(typeof(EntryCustomRenderer), typeof(OnegaiMuscle.Platforms.Android.Renderer.MyEntryRenderer));
+            })
+
 #endif
 
 #if IOS
-                        handlers.AddCompatibilityRenderer(typeof(EntryCustomRenderer), typeof(OnegaiMuscle.Platforms.iOS.Renderer.MyEntryRenderer));
+.UseMauiCompatibility()
+            .ConfigureMauiHandlers((handlers) =>
+            {
+                handlers.AddCompatibilityRenderer(typeof(EntryCustomRenderer), typeof(OnegaiMuscle.Platforms.iOS.Renderer.MyEntryRenderer));
+            })
 #endif
-            });
+            ;
         builder.UseMauiApp<App>().UseMauiCommunityToolkit();
 
-		builder.Services.AddSingleton<VerificationPage>();
-		builder.Services.AddSingleton<VerificationViewModel>();
+        builder.Services.AddSingleton<VerificationPage>();
+        builder.Services.AddSingleton<VerificationViewModel>();
 
-		builder.Services.AddTransient<BookingPage>();
-		builder.Services.AddTransient<BookingViewModel>();
+        builder.Services.AddTransient<BookingPage>();
+        builder.Services.AddTransient<BookingViewModel>();
 
         return builder.Build();
-	}
+    }
 }
